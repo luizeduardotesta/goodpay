03aafd63854143b17a83d384e4b9bec80d4aef4c
luizeduardotesta <luizsonecazzz@hotmail.com>
2023-03-10 14:33:35 -0300

    add the full project

--- scripts/controleFinanceiro.js
+++ scripts/controleFinanceiro.js
@@ -1,39 +1,42 @@
+
 let titulo;
 let total;
 let tipo;
-let bntControleFinanceiro;
+let btnControleFinanceiro;
 let uldashboard;
 
-titulo = document.getElementById("titulo");
-total = document.getElementById("total");
-tipo = document.getElementById("tipo");
-bntControleFinanceiro = document.getElementById("bntControleFinanceiro");
-uldashboard = document.getElementById("dashboard");
+titulo = document.getElementById('titulo');
+total = document.getElementById('total');
+tipo = document.getElementById('tipo');
+btnControleFinanceiro = document.getElementById('btnControleFinanceiro');
+uldashboard = document.getElementById('dashboard');
 
 total.addEventListener("input", () => {
-    if (total.value < 0) {
-        alert(`O valor informado não precisa ser negativo para reprezentar uma despesa. \n Informe um valor maior do que zero.`);
+    if (total.value < 0){
+        alert(`O valor informado não precisa ser negativo para representar uma despesa.\n Informe um valor maior que zero.`);
         total.value = '';
     }
 });
 
-bntControleFinanceiro.addEventListener("click", () => {
-    if (titulo.value === '' || total.value === '' || tipo.value === 'selecione') {
-        alert("Existem campos não preenchidos");
+/* Adiciona o evento clique no botão Adicionar */
+btnControleFinanceiro.addEventListener("click", () => {
+    /*  */
+    if (titulo.value === '' || tipo.value === 'Selecione' || total.value === ''){
+        alert(`Existem campos não preenchidos.`);
     } else {
-        if (tipo.value === "receita") {
-            uldashboard.innerHTML +=
-        `
-        <li>${titulo.value.capitalize()}</li>
-        <li id="receita">+ R$ ${total.value}</li>
-        `
+        if (tipo.value === "Receita") {
+            uldashboard.innerHTML += 
+            `
+            <li>${titulo.value.capitalize()}</li>
+            <li id="receita">+ R$ ${total.value}</li>
+            `
             limpar();
         } else {
             uldashboard.innerHTML +=
-        `
-        <li>${titulo.value.capitalize()}</li>
-        <li id="despesa">- R$ ${total.value}</li>
-        `
+            `
+            <li>${titulo.value.capitalize()}</li>
+            <li id="despesa">- R$ ${total.value}</li>
+            `
             limpar();
         }
     }
@@ -42,7 +45,7 @@ bntControleFinanceiro.addEventListener("click", () => {
 function limpar() {
     titulo.value = '';
     total.value = '';
-    tipo.value = 'selecione';
+    tipo.value = 'Selecione';
 }
 
 String.prototype.capitalize = function() {
--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,43 @@
-// Banco de dados fake
-
-const usuarios = [
-    {
-        nomeUsuario: "Gregory",
-        email: "email@email.com",
-        senha: "123456",
-    },
-    {
-        nomeUsuario: "Luna",
-        email: "email1@email.com",
-        senha: "123456", 
-    },
-];
-
-// Dados de Loggin
+import usuarios from "./usuarios.js";
+// Dados de entrada de login
 const campoEmail = document.getElementById("email");
 const campoSenha = document.getElementById("senha");
 
 // Botão de entrar
 const btnEntrar = document.getElementById("btn-entrar");
 
-// Ouvinte do evento
+// Ouvinte de evento
+const captcha = document.getElementById("resposta");
+const validacao = document.getElementById("aleatorio");
+let num1 = Math.floor(Math.random() * 10);
+let num2 = Math.floor(Math.random() * 10);
+validacao.innerHTML = (`Resolver a operação: ${num1} + ${num2} =`);
+let resposta = num1 + num2;
+
 btnEntrar.addEventListener("click", () => {
-    // 1 - capturar os dados dos imputs
-    const email = campoEmail.value;
-    const senha = campoSenha.value;
+  const email = campoEmail.value;
+  const senha = campoSenha.value;
+  const certa = parseInt(captcha.value);
 
-    // 2 - checar se o email do usuario existe
-    // find: percorre o array até encontrar o elemento procurado
-    let usuarioValido = usuarios.find((usuario) => usuario.email === email);
-    
-    if (usuarioValido !== undefined){
-        // 3 - checar se a senha está correta
-        if(usuarioValido.senha === senha){
-            window.location.pathname = "/app.html";
-        }else{
-            alert("Senha incorreta!");
-        }
-    }else{
-        alert("Usuario não existe!");
+  let usuarioValido = usuarios.find((usuario) => usuario.email === email);
+
+  if (usuarioValido !== undefined) {
+    if (usuarioValido.senha === senha) {
+      if (certa === resposta){
+        localStorage.setItem("acesso", true);
+        window.location.pathname = "/app.html"
+      } else{
+        alert("rever resposta")
+      }
+      
+    } else {
+      alert("Senha incorreta!");
     }
-
-})
+  } else {
+    alert("Usuário não existe!");
+  }
+});
+const acesso = localStorage.getItem("acesso");
+if (acesso === "true") {
+  window.location.pathname = "/app.html";
+}
